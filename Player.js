1b5be5ccb3dc7851e7b38b1b4e5ecb3291e1fea0
Manu De Buck <manu@mdebuck.org>
2023-03-29 15:48:10 +0200

    :bug: Fix warping bug of logo

--- Player.js
+++ Player.js
@@ -10,8 +10,8 @@ export default class Player {
 
     this.x = this.canvas.width / 2;
     this.y = this.canvas.height - 75;
-    this.width = 50;
-    this.height = 50;
+    this.width = 59;
+    this.height = 37;
     this.image = new Image();
     this.image.src = "images/logo.svg";
 
